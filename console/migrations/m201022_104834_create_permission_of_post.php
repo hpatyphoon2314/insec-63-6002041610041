b4354b06be90f08f0f392c74e3cc514c8018e378
hpatyphoon2314 <48791618+hpatyphoon2314@users.noreply.github.com>
2020-10-22 18:24:19 +0700

    create permission of post

--- console/migrations/m201022_104834_create_permission_of_post.php
+++ console/migrations/m201022_104834_create_permission_of_post.php
@@ -12,7 +12,28 @@ class m201022_104834_create_permission_of_post extends Migration
      */
     public function safeUp()
     {
+        $auth = Yii::$app->authManager;
 
+        $create = $auth->createPermission('post-create');
+        $create->description = 'Create a post';
+        $auth->add($create);
+
+        $index = $auth->createPermission('post-index');
+        $index->description = 'List a post';
+        $auth->add($index);
+
+        $delete = $auth->createPermission('post-delete');
+        $delete ->description = 'Delete a post';
+        $auth->add($delete);
+
+        $update = $auth->createPermission('post-update');
+        $update  ->description = 'Update a post';
+        $auth->add($update);
+
+        $view = $auth->createPermission('post-view');
+        $view  ->description = 'View a post';
+        $auth->add($view);
+        
     }
 
     /**
@@ -20,9 +41,33 @@ class m201022_104834_create_permission_of_post extends Migration
      */
     public function safeDown()
     {
-        echo "m201022_104834_create_permission_of_post cannot be reverted.\n";
+        $auth = Yii::$app->authManager;
 
-        return false;
+		$create = $auth->getPermission('post-create');
+		if ($create){
+			$auth->remove($create);
+        }
+        $index = $auth->getPermission('post-index');
+		if ($index){
+			$auth->remove($index);
+        }
+        $delete = $auth->getPermission('post-delete');
+		if ($delete){
+			$auth->remove($delete);
+        }
+        $update = $auth->getPermission('post-update');
+		if ($update){
+			$auth->remove($update);
+        }
+        $view= $auth->getPermission('post-view');
+		if ($view){
+			$auth->remove($view);
+        }
+
+
+
+
+        return true;
     }
 
     /*
